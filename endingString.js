79a9257e6d7dcc625f60007083a622b477f28c16
jestintab <jestintab@gmail.com>
2016-02-08 12:50:38 +0530

    some old change

---
+++ endingString.js
@@ -0,0 +1,18 @@
+function end(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  for(var i=0; i < str.length; i++){
+  	if(str[str.length-1] == target){
+  		return true;
+  		console.log(true);
+  	}
+  	return false;
+  		console.log(false);
+
+  }
+  		console.log('works');
+
+  return str;
+}
+
+end("Bastian", "n");
--- largenumarr.js
+++ largenumarr.js
@@ -1,7 +1,16 @@
 function largestOfFour(arr) {
-  // You can do this!
-  console.log('works');
-  //return arr;
+	var largeArr =[];
+	var largeNum =0;
+ for(var i =0; i < arr.length; i++){
+ 	for( var j = 0; j < arr[i].length; j++ ){
+ 		if( largeNum < arr[i][j]){
+ 			largeArr[i] = arr[i][j];
+ 			largeNum = arr[i][j];
+ 		}		
+ 	}largeNum =0;
+ }
+  console.log(largeArr);
+  return largeArr;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
